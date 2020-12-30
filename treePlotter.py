f567a7da5501b9f16951d0c2b103ab86568b3212
Administrator <Administrator@PC-20181026RGWT>
2020-12-30 23:25:55 +0800

    12_30

--- treePlotter.py
+++ treePlotter.py
@@ -1,44 +1,86 @@
 import matplotlib.pyplot as plt
 
-#定义文本框和箭头格式
-decisionNode=dict(boxstyle="sawtooth",fc="0.8")
-leafNode=dict(boxstyle="round4",fc="0.8")
-arrow_arge=dict(arrowstyle="<-")
+# 定义文本框和箭头格式
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_arge = dict(arrowstyle="<-")
+dataTest = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+            {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}]
 
-#获取叶节点的数目
-def getNumLeafs(myTree):
-    numLeafs=0
-    firstStr=myTree.keys()[0]
+
+# 在父子节点填充文本信息
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0]-cntrPt[0])/2.0+cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0+cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt=(plotTree.xOff+(1.0+float(numLeafs))/2.0/plotTree.totalW,plotTree.yOff)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr,cntrPt,parentPt,decisionNode)
     secondDict=myTree[firstStr]
-    for key in secondDict.keys()[0]:
-        if(type(secondDict[key])._name_=='dict'):
-            numLeafs+=getNumLeafs(secondDict[key])
-        else: numLeafs+=1
+    plotTree.yOff=plotTree.yOff-1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if(type(secondDict[key]).__name__=='dict'):
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xOff=plotTree.xOff+1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+            plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+    plotTree.yOff=plotTree.yOff+1.0/plotTree.totalD
+
+
+# 获取叶节点的数目
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if(type(secondDict[key]).__name__ == 'dict'):
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
     return numLeafs
 
-#获取树的层数
+
+# 获取树的层数
 def getTreeDepth(myTree):
-    maxDepth=0
-    firstStr=myTree.keys()[0]
-    secondDict=myTree[firstStr]
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
     for key in secondDict.keys():
-        if(type(secondDict[key])._name_=='dict'):
-            thisDepth=1+getTreeDepth(secondDict[key])
-        else: thisDepth=1
-        if(thisDepth>maxDepth): maxDepth=thisDepth
+        if(type(secondDict[key]).__name__ == 'dict'):
+            thisDepth = 1+getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if(thisDepth > maxDepth):
+            maxDepth = thisDepth
     return maxDepth
 
-#绘制带箭头的注解
-def plotNode(nodeTxt,centerPt,parentPt,nodeType):
-    createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',xytext=centerPt,textcoords='axes fraction',
-    va="center",ha="center",bbox=nodeType,arrowprops=arrow_arge)
 
-def createPlot():
-    fig=plt.figure(1,facecolor='white')
+# 绘制带箭头的注解
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    #：在改程序中使用了变量createPlot.ax1这个变量，这是一种全局变量的表达方式。
+    # 在两个子函数下的变量本来都是局部变量，不能跨出变量的作用域使用，此处使用“函数名.变量名”的方式，将变量申明为全局变量。
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_arge)
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
     fig.clf()
-    createPlot.ax1=plt.subplot(111,frameon=False)
-    plotNode('a decison node',(0.5,0.1),(0.1,0.5),decisionNode)
-    plotNode('a leaf node',(0.8,0.1),(0.3,0.8),leafNode)
+    axprops=dict(xticks=[],yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False,**axprops)
+    plotTree.totalW=float(getNumLeafs(inTree))
+    plotTree.totalD=float(getTreeDepth(inTree))
+    plotTree.xOff=-0.5/plotTree.totalW
+    plotTree.yOff=-1.0
+    plotTree(inTree,(0.5,1.0),'')
     plt.show()
 
-createPlot()
+
+createPlot(dataTest[0])
